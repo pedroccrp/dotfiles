d282028365692858af3ca91d2181e337ad1226e0
Pedro Pontes <pontescpedro@gmail.com>
2025-03-26 10:33:10 -0300

    Run updates on start

--- nvim/lua/plugins-installer.lua
+++ nvim/lua/plugins-installer.lua
@@ -4,3 +4,11 @@ require("plugins.flutter-plugins")
 require("plugins.random-plugins")
 require("plugins.git-plugins")
 require("plugins.ai-plugins")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  callback = function()
+    vim.cmd("PackerInstall")
+    vim.cmd("TSUpdate")
+  end,
+})
--- nvim/lua/plugins/random-plugins.lua
+++ nvim/lua/plugins/random-plugins.lua
@@ -76,12 +76,6 @@ use({
 
 use({
   "iamcco/markdown-preview.nvim",
-  run = function()
-    vim.fn["mkdp#util#install"]()
-  end,
-})
-use({
-  "iamcco/markdown-preview.nvim",
   run = "cd app && npm install",
   setup = function()
     vim.g.mkdp_filetypes = {
