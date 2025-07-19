9bdb2fc91de4092385a9c9d1e0b19b8a2d2a3318
pedroccrp <pontescpedro@gmail.com>
2025-07-19 14:07:03 -0300

    Add new bindings

--- zsh/.bindings.zsh
+++ zsh/.bindings.zsh
@@ -1,5 +1,9 @@
 bindkey -v
 
+# Used to remove the delay when pressing escape
+# but can break other keybindings, make sure to check
+KEYTIMEOUT=5
+
 # Emacs-style bindings in vi insert mode
 bindkey -M viins '^A' beginning-of-line
 bindkey -M viins '^E' end-of-line
@@ -22,5 +26,15 @@ zle -N down-line-or-beginning-search
 
 bindkey -M viins '^[[A' up-line-or-beginning-search
 bindkey -M viins '^[[B' down-line-or-beginning-search
+bindkey -M viins '' up-line-or-beginning-search
+bindkey -M viins '' down-line-or-beginning-search
+bindkey -M viins '^[k' up-line-or-beginning-search
+bindkey -M viins '^[j' down-line-or-beginning-search
 
 bindkey -M viins '^R' fzf-history-widget
+
+# Autosuggest
+bindkey -M viins '^@' autosuggest-accept
+bindkey -M viins '' autosuggest-accept
+bindkey -M viins '' undefined-key
+bindkey -M viins '' forward-word
