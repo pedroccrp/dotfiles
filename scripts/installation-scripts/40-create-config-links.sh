982e2b48376af4f6be434c131c63e647b7d85f1e
pedroccrp <pontescpedro@gmail.com>
2025-07-23 19:16:29 -0300

    Add ideavim to link script

--- scripts/installation-scripts/40-create-config-links.sh
+++ scripts/installation-scripts/40-create-config-links.sh
@@ -3,6 +3,8 @@ DOTFILES=$HOME/dotfiles
 ln -sf $DOTFILES/tmux/.tmux.conf $HOME/.tmux.conf
 if [ -d $HOME/.config/nvim ]; then rm -rf $HOME/.config/nvim; fi
 ln -sf $DOTFILES/nvim $HOME/.config/nvim
+if [ -d $HOME/.ideavimrc ]; then rm -rf $HOME/.ideavimrc; fi
+ln -sf $DOTFILES/ideavim/.ideavimrc $HOME/.ideavimrc
 ln -sf $DOTFILES/zsh/.zshrc $HOME/.zshrc
 if [ -d $HOME/.config/kitty ]; then rm -rf $HOME/.config/kitty; fi
 ln -sf $DOTFILES/kitty $HOME/.config/kitty
