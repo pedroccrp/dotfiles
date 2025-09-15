eef994922a03e4ea099395c0d917bb344c198e39
pedroccrp <pontescpedro@gmail.com>
2025-09-15 04:11:49 -0300

    Fix installation script

--- scripts/installation-scripts/10-install-packages.sh
+++ scripts/installation-scripts/10-install-packages.sh
@@ -4,7 +4,7 @@ if [ $(id -u) -ne 0 ]
 fi
 
 base_packages=(
-	base-devel
+  base-devel
   xdg-desktop-portal
   xdg-desktop-portal-gtk
   xdg-desktop-portal-hyprland
@@ -26,17 +26,17 @@ network_packages=(
 )
 
 bluetooth_packages=(
-	bluez
-	bluez-utils
-	bluez-deprecated-tools
+  bluez
+  bluez-utils
+  bluez-deprecated-tools
 )
 
 audio_pakages=(
-	alsa-lib
-	alsa-utils
-	pipewire
-	pipewire-pulse
-	pavucontrol
+  alsa-lib
+  alsa-utils
+  pipewire
+  pipewire-pulse
+  pavucontrol
 )
 
 desktop_packages=(
@@ -85,19 +85,20 @@ font_packages=(
 packages=(
   grim
   vlc
-	bitwarden-cli
+  bitwarden-cli
 )
 
 echo "Installing packages..."
-yes | pacman -S --needed ${base_packages[@]} ${network_packages[@]} ${bluetooth_packages[@]} ${audio_pakages[@]} ${desktop_packages[@]} ${dev_packages[@]} ${font_packages[@]} ${packages[@]}
+yes | pacman -S --noconfirm --needed ${base_packages[@]} ${network_packages[@]} ${bluetooth_packages[@]} ${audio_pakages[@]} ${desktop_packages[@]} ${dev_packages[@]} ${font_packages[@]} ${packages[@]}
 
 # Install YAY
 if ! command -v yay >/dev/null 2>&1; then
     echo "Installing yay..."
     git clone https://aur.archlinux.org/yay.git /tmp/yay &>/dev/null
-    pushd /tmp/yay &>/dev/null
+    cd /tmp/yay &>/dev/null
+    chown $USER:$USER . -R
     makepkg -si 2>/dev/null
-    popd &>/dev/null
+    cd - &>/dev/null
     yay -Syu
 else
     echo "Yay already installed!"
