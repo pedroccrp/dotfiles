cb59d87bcbd752c29b5bb8d31e58429231b082f3
pedroccrp <pontescpedro@gmail.com>
2025-09-17 22:08:30 -0300

    Add packages to install script

--- scripts/installation-scripts/10-install-packages.sh
+++ scripts/installation-scripts/10-install-packages.sh
@@ -11,6 +11,7 @@ base_packages=(
   unzip
   zip
   xclip
+  xorg-xinput
   wl-clipboard
   fzf
   ripgrep
@@ -18,6 +19,7 @@ base_packages=(
   lsof
   rsync
   reflector
+  htop
 )
 
 network_packages=(
