aef7da20bcd1eaf720d7a49403ae2ed298961082
pedroccrp <pontescpedro@gmail.com>
2025-10-08 00:50:24 -0300

    Add git config for rebase always

--- scripts/installation-scripts/30-prepare-git.sh
+++ scripts/installation-scripts/30-prepare-git.sh
@@ -4,3 +4,5 @@ gh auth login -p ssh -h GitHub.com
 git config --global user.email "pontescpedro@gmail.com"
 git config --global user.name "pedroccrp"
 git config --global core.editor "nvim"
+
+git config --global pull.rebase true
