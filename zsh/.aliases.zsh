0f9d4e7b4c4acad6647e092d498c4402c2cb3db8
pedroccrp <pontescpedro@gmail.com>
2025-12-08 16:23:25 -0300

    Add translate method for oxean api

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -80,3 +80,11 @@ tdefault() {
   tmuxp load -d $DOTFILES/tmux/sessions/_default.yaml > /dev/null
   tmux attach -t Default
 }
+
+translate_oxean_api() {
+  local password="$1"
+  echo 'Vamos traduzir tudo para você :D'
+  export GOOGLE_TRANSLATE_API_KEY="$password"
+  RAILS_ENV=development i18n-tasks translate-missing --from en de, es, fr, nb, pt-BR, zh-CN, th;
+  sed -i "s/\b'\b/'/g" config/locales/fr.yml # Replace ' to ’ in french
+}
