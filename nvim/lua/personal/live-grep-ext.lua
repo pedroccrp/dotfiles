1bce9182a5f8bcc42e278f1be94d05847b6a714c
pedroccrp <pontescpedro@gmail.com>
2026-01-09 11:25:07 -0300

    Add extension search to live grep telescope

---
+++ nvim/lua/personal/live-grep-ext.lua
@@ -0,0 +1,67 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local make_entry = require("telescope.make_entry")
+local sorters = require("telescope.sorters")
+local utils = require("telescope.utils")
+
+local flatten = vim.tbl_flatten
+
+local function has_rg_program(name, program)
+  if vim.fn.executable(program) == 1 then
+    return true
+  end
+  vim.notify(name .. " requires ripgrep (rg)", vim.log.levels.ERROR)
+  return false
+end
+
+local M = {}
+
+M.live_grep_ext = function(opts)
+  opts = opts or {}
+
+  local vimgrep_arguments = opts.vimgrep_arguments or conf.vimgrep_arguments
+  if not has_rg_program("live_grep_ext", vimgrep_arguments[1]) then
+    return
+  end
+
+  opts.cwd = opts.cwd and utils.path_expand(opts.cwd) or vim.loop.cwd()
+
+  local args = flatten({ vimgrep_arguments })
+
+  local live_grepper = finders.new_job(function(prompt)
+    if type(prompt) ~= "string" or prompt == "" then
+      return nil
+    end
+
+    local text, ext = prompt:match("^(.-)%s%s(%S+)$")
+
+    local final_prompt = prompt
+    local final_args = args
+
+    if text and ext and not text:match("%s$") then
+      final_prompt = text
+      final_args = vim.list_extend(vim.deepcopy(args), {
+        "--glob",
+        "*." .. ext,
+      })
+    end
+
+    return flatten({
+      final_args,
+      "--",
+      final_prompt,
+    })
+  end, make_entry.gen_from_vimgrep(opts), opts.max_results, opts.cwd)
+
+  pickers
+    .new(opts, {
+      prompt_title = "Live Grep (text␠␠ext)",
+      finder = live_grepper,
+      previewer = conf.grep_previewer(opts),
+      sorter = sorters.get_generic_fuzzy_sorter(opts),
+    })
+    :find()
+end
+
+return M
