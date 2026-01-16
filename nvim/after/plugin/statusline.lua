1236ad959fa9cf49e8373dc619fe776c7f769072
pedroccrp <pontescpedro@gmail.com>
2026-01-16 12:14:30 -0300

    Add navic to status line and improve display

--- nvim/after/plugin/statusline.lua
+++ nvim/after/plugin/statusline.lua
@@ -1,6 +1,70 @@
-require("lualine").setup({
+local navic = require("nvim-navic")
+navic.setup({
+  highlight = true,
+  separator = " > ",
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.server_capabilities.documentSymbolProvider then
+      navic.attach(client, args.buf)
+    end
+  end,
+})
+
+local lualine = require("lualine")
+lualine.setup({
   options = {
     icons_enabled = true,
     theme = "auto",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    always_show_tabline = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+      refresh_time = 16, -- ~60fps
+      events = {
+        "WinEnter",
+        "BufEnter",
+        "BufWritePost",
+        "SessionLoadPost",
+        "FileChangedShellPost",
+        "VimResized",
+        "Filetype",
+        "CursorMoved",
+        "CursorMovedI",
+        "ModeChanged",
+      },
+    },
   },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "diagnostics", "filename" },
+    lualine_c = { "require'nvim-navic'.get_location()" },
+    lualine_x = { "searchcount", "selectioncount" },
+    lualine_y = { "filetype", "progress" },
+    lualine_z = { "location" },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {},
 })
