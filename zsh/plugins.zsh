549fb985d5cf4569e6d0555af0000add8df38abe
pedroccrp <pontescpedro@gmail.com>
2026-01-18 12:59:37 -0300

    Add angular completions

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -9,6 +9,9 @@ source $HOME/.local/share/zsh/plugins/flatpak-zsh-completion/flatpak.plugin.zsh
 source /usr/share/fzf/key-bindings.zsh
 source /usr/share/fzf/completion.zsh
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Personal ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Frameworks ~~~~~~~~~~~~~~~~~~~~~
+source <(ng completion script)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Personal ~~~~~~~~~~~~~~~~~~~~~~
 source $HOME/dotfiles/zsh/custom-plugins/vi-mode.zsh
 source $HOME/dotfiles/zsh/custom-plugins/tmux-session-kill.zsh
