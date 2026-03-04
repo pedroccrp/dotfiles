6b5d68e4785d3cc103f514bc2d531bc636cf14a4
pedroccrp <pontescpedro@gmail.com>
2026-03-04 08:46:48 -0300

    Paint color hex background on css files

--- nvim/after/plugin/ui/colorizer.lua
+++ nvim/after/plugin/ui/colorizer.lua
@@ -3,4 +3,12 @@ local helpers = require("helpers")
 local colorizer = helpers.safe_require("colorizer")
 if not colorizer then return end
 
-colorizer.setup()
+colorizer.setup(nil, {
+  names = false, -- "Name" codes like Blue or blue
+  RGB = true, -- #RGB hex codes
+  RRGGBB = true, -- #RRGGBB hex codes
+  RRGGBBAA = true, -- #RRGGBBAA hex codes
+  css = { css = true }, -- Enable all CSS features: rgb(), hsl(), etc.
+  scss = { css = true }, -- Enable all SCSS features: rgb(), hsl(), etc.
+  html = { css = true }, -- Enable all HTML features: rgb(), hsl(), etc.
+})
