43e58c9b2f0aa3b61b52899eb94dcd4c6d6ef811
pedroccrp <pontescpedro@gmail.com>
2026-03-15 13:13:58 -0300

    Add colors to grep

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -19,4 +19,8 @@ alias gcm="git commit"
 alias gpp="git push origin HEAD"
 alias gpl="git pull"
 
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
 alias bw='/usr/bin/node /usr/lib/node_modules/@bitwarden/cli/build/bw.js'
