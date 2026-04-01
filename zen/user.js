b6b08969f3313eae323723d401b94fea23575c74
pedroccrp <pontescpedro@gmail.com>
2026-04-01 12:02:57 -0300

    Add zen prefs

---
+++ zen/user.js
@@ -0,0 +1,6 @@
+user_pref("zen.view.sidebar-expanded", false);
+user_pref("zen.view.show-newtab-button-top", false);
+user_pref("zen.view.compact.hide-toolbar", true);
+user_pref("zen.view.compact.enable-at-startup", false);
+user_pref("zen.view.use-single-toolbar", false);
+user_pref("zen.welcome-screen.seen", true);
