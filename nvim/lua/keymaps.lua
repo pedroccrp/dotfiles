4adbcd1f468aa2c15034269fa45c4ffe22bade00
pedroccrp <pontescpedro@gmail.com>
2026-04-06 09:45:40 -0300

    Add description to nvim keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,51 +1,36 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
--- The ':' is to weird to use on the keyboard
-vim.keymap.set({ 'n', 'v' }, '<leader>e', ':')
+vim.keymap.set({ "n", "v" }, "<leader>e", ":", { desc = "Enter command mode" })
 
-vim.keymap.set('n', '<leader>ve', ':e ~/.config/nvim/init.lua<CR>', { silent = true })
--- vim.keymap.set('n', '<leader>vs', ':source ~/.config/nvim/init.lua<CR>', { silent = true })
+vim.keymap.set("n", "<leader>k", ":nohlsearch<CR>", { silent = true, desc = "Clear search highlight" })
+vim.keymap.set("n", "<leader>x", ":!xdg-open %<cr><cr>", { desc = "Open file externally" })
 
-vim.keymap.set('n', '<leader>k', ':nohlsearch<CR>', { silent = true })
+vim.keymap.set("n", "yp", ":let @+=expand('%').':'.line('.')<CR>", { silent = true, desc = "Copy file path with line number" })
 
-vim.keymap.set('n', 'yp', ':let @+=expand(\'%\').\':\'.line(\'.\')<CR>', { silent = true })
+vim.keymap.set("", "gf", ":edit <cfile><CR>", { silent = true, desc = "Open file under cursor (create if missing)" })
 
--- Allow gf to open non-existent files
-vim.keymap.set('', 'gf', ':edit <cfile><CR>', { silent = true })
+vim.keymap.set("v", "<", "<gv", { silent = true, desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { silent = true, desc = "Indent right and reselect" })
 
--- Reselect visual selection after indenting
-vim.keymap.set('v', '<', '<gv', { silent = true })
-vim.keymap.set('v', '>', '>gv', { silent = true })
+vim.keymap.set("v", "y", "myy`y", { silent = true, desc = "Yank without moving cursor" })
+vim.keymap.set("v", "Y", "myY`y", { silent = true, desc = "Yank line without moving cursor" })
 
--- Maintain the cursor position when yanking a visual selection
--- http://ddrscott.github.io/blog/2016/yank-without-jank/
-vim.keymap.set('v', 'y', 'myy`y', { silent = true })
-vim.keymap.set('v', 'Y', 'myY`y', { silent = true })
+vim.keymap.set("n", "x", '"_x', { desc = "Delete without affecting registers" })
 
--- Don't override clipboard when deleting characters
-vim.keymap.set('n', 'x', '"_x')
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true, desc = "Move up by display line" })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true, desc = "Move down by display line" })
 
--- When text is wrapped, move by terminal rows, not lines, unless a count is provided
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("v", "p", '"_dP', { silent = true, desc = "Paste without overwriting register" })
 
--- Paste replace visual selection without copying it
-vim.keymap.set('v', 'p', '"_dP', { silent = true })
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { silent = true, desc = "Increase window height" })
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { silent = true, desc = "Decrease window height" })
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { silent = true, desc = "Decrease window width" })
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { silent = true, desc = "Increase window width" })
 
--- Open the current file in the default program (on Mac this should just be just `open`)
-vim.keymap.set('n', '<leader>x', ':!xdg-open %<cr><cr>')
+vim.keymap.set("i", "<A-j>", "<Esc>:move .+1<CR>==gi", { silent = true, desc = "Move line down" })
+vim.keymap.set("i", "<A-k>", "<Esc>:move .-2<CR>==gi", { silent = true, desc = "Move line up" })
+vim.keymap.set("x", "<A-j>", ":move '>+1<CR>gv-gv", { silent = true, desc = "Move selection down" })
+vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv", { silent = true, desc = "Move selection up" })
 
--- Resize with arrows
-vim.keymap.set('n', '<C-Up>', ':resize +2<CR>', { silent = true })
-vim.keymap.set('n', '<C-Down>', ':resize -2<CR>', { silent = true })
-vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', { silent = true })
-vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', { silent = true })
-
--- Move text up and down
-vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi', { silent = true })
-vim.keymap.set('i', '<A-k>', '<Esc>:move .-2<CR>==gi', { silent = true })
-vim.keymap.set('x', '<A-j>', ":move '>+1<CR>gv-gv", { silent = true })
-vim.keymap.set('x', '<A-k>', ":move '<-2<CR>gv-gv", { silent = true })
-
-vim.keymap.set('n', '<BS>', ':e#<CR>', { silent = true, desc = "Go to alternate buffer"})
+vim.keymap.set("n", "<BS>", ":e#<CR>", { silent = true, desc = "Go to alternate buffer" })
