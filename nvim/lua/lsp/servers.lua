fd4eb482848d7a3a823eefd81d102412f1c5895b
pedroccrp <pontescpedro@gmail.com>
2026-04-07 15:45:49 -0300

    Add helper method on lsp servers

--- nvim/lua/lsp/servers.lua
+++ nvim/lua/lsp/servers.lua
@@ -1,34 +1,33 @@
-vim.lsp.config("ruby_lsp", {
+local function setup(server, opts)
+  opts = opts or {}
+  vim.lsp.config(server, opts)
+  vim.lsp.enable(server)
+end
+
+setup("ruby_lsp", {
   cmd = { "ruby", "-S", "ruby-lsp" },
   filetypes = { "ruby", "eruby" },
 })
 
-vim.lsp.config("bashls", {
+setup("bashls", {
   filetypes = { "sh", "bash", "zsh" },
 })
 
-vim.lsp.config("lua_ls", {
+setup("lua_ls", {
   settings = {
     Lua = {
       diagnostics = { globals = { "vim" } },
     },
   },
-  filetypes = { "lua" },
 })
 
-vim.lsp.config("gdscript", {
+setup("gdscript", {
   cmd = { "nc", "localhost", "6005" },
   filetypes = { "gd", "gdscript", "gdscript3" },
   root_markers = { "project.godot", ".git" },
 })
 
-vim.lsp.config("dartls", {
+setup("dartls", {
   cmd = { "dart", "language-server", "--protocol=lsp" },
-  filetypes = { "dart" },
   root_markers = { "pubspec.yaml", ".git" },
 })
-
-vim.lsp.enable("ruby_lsp")
-vim.lsp.enable("lua_ls")
-vim.lsp.enable("gdscript")
-vim.lsp.enable("dartls")
