a9609d62967391436b9345606c76e12abbc1a29c
pedroccrp <pontescpedro@gmail.com>
2026-04-07 15:57:28 -0300

    Redefine lsp keymaps with telescope

--- nvim/lua/lsp/keymaps.lua
+++ nvim/lua/lsp/keymaps.lua
@@ -1,3 +1,9 @@
+local ok, telescope = pcall(require, "telescope.builtin")
+
+local refs = ok and telescope.lsp_references or vim.lsp.buf.references
+local defs = ok and telescope.lsp_definitions or vim.lsp.buf.definition
+local impl = ok and telescope.lsp_implementations or vim.lsp.buf.implementation
+
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(event)
     local buffer = event.buf
@@ -7,9 +13,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
     vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
-    -- vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
-    -- vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, opts)
-    -- vim.keymap.set("n", "gi", require("telescope.builtin").lsp_implementations, opts)
+    vim.keymap.set("n", "grr", refs, opts)
+    vim.keymap.set("n", "gd", defs, opts)
+    vim.keymap.set("n", "gi", impl, opts)
 
     vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
