0731cf8295ba0ca5a2700562f35af0702261ac76
Pedro Pontes <pontescpedro@gmail.com>
2026-04-10 00:05:21 -0300

    Improve git/github configure script

--- scripts/installation-scripts/03-configuration/10-configure-git.zsh
+++ scripts/installation-scripts/03-configuration/10-configure-git.zsh
@@ -2,11 +2,19 @@
 
 set -euo pipefail
 
-echo "Logging into gh..."
-gh auth login -p ssh -h GitHub.com
+if gh auth status --hostname github.com >/dev/null 2>&1; then
+  echo "Already logged to gh!"
+else
+  echo "Logging into gh..."
+  gh auth login -p ssh -h GitHub.com
+fi
+
+echo "Setting global configs for git..."
 
 git config --global user.email "pontescpedro@gmail.com"
 git config --global user.name "Pedro Pontes"
 git config --global core.editor "nvim"
 
 git config --global pull.rebase true
+
+echo "Done."
