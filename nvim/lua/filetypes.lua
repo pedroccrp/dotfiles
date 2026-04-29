af06c57d0b6a84d52afb5ce4aaa5ec7a7d64f3a2
Pedro Pontes <pontescpedro@gmail.com>
2026-04-29 10:30:16 -0300

    Add kotlin filetype options

--- nvim/lua/filetypes.lua
+++ nvim/lua/filetypes.lua
@@ -1,30 +1,31 @@
 local filetype_map = {
-    text = { 'txt', 'log' },
-    markdown = { 'md', 'markdown' },
-    python = { 'pyw' },
-    javascript = { 'jsx' },
-    javascriptreact = { 'jsx' },
-    typescript = { 'tsx' },
-    typescriptreact = { 'tsx' },
-    html = { 'htm' },
-    tmux = { 'tmux.conf' },
-    gdscript = { 'gd', 'gdscript', 'gdscript3' },
+  text = { "txt", "log" },
+  markdown = { "md", "markdown" },
+  python = { "pyw" },
+  javascript = { "jsx" },
+  javascriptreact = { "jsx" },
+  typescript = { "tsx" },
+  typescriptreact = { "tsx" },
+  html = { "htm" },
+  tmux = { "tmux.conf" },
+  gdscript = { "gd", "gdscript", "gdscript3" },
+  kotlin = { "kt" },
 }
 
 local function set_filetype()
-    local filename = vim.fn.expand('%:t')
+  local filename = vim.fn.expand("%:t")
 
-    for filetype, extensions in pairs(filetype_map) do
-        for _, ext in ipairs(extensions) do
-          if filename:sub(-#ext) == ext then
-            vim.bo.filetype = filetype
-            return
-          end
-        end
+  for filetype, extensions in pairs(filetype_map) do
+    for _, ext in ipairs(extensions) do
+      if filename:sub(-#ext) == ext then
+        vim.bo.filetype = filetype
+        return
+      end
     end
+  end
 end
 
 vim.api.nvim_create_autocmd("BufRead", {
-    pattern = "*",
-    callback = set_filetype,
+  pattern = "*",
+  callback = set_filetype,
 })
