a713687d9e0d9397245605fe4b2880cdebdbaca7
Pedro Pontes <pontescpedro@gmail.com>
2026-05-03 20:39:11 -0300

    Apply formatter on options.lua

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -21,10 +21,10 @@ vim.opt.wrap = false
 vim.opt.breakindent = true
 
 vim.opt.list = true
-vim.opt.listchars = { tab = '▸ ', trail = '·' }
-vim.opt.fillchars:append({ eob = ' ' })
+vim.opt.listchars = { tab = "▸ ", trail = "·" }
+vim.opt.fillchars:append({ eob = " " })
 
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 vim.opt.splitbelow = true
 vim.opt.splitright = true
@@ -32,22 +32,22 @@ vim.opt.splitright = true
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 
 vim.opt.confirm = true
 vim.opt.undofile = true
 
 vim.opt.backup = true
-vim.opt.backupdir:remove('.')
+vim.opt.backupdir:remove(".")
 vim.opt.shortmess:append({ I = true })
 
-vim.opt.wildmode = 'longest:full,full'
+vim.opt.wildmode = "longest:full,full"
 vim.opt.showmode = false
 
 vim.opt.updatetime = 4001
 vim.opt.redrawtime = 10000
 
-vim.opt.signcolumn = 'yes:2'
-vim.opt.colorcolumn = '80'
+vim.opt.signcolumn = "yes:2"
+vim.opt.colorcolumn = "80"
 
 vim.opt.timeout = false
