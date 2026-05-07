cb4ee09d6f25f3db276871322c47d22424ed5dc8
Pedro Pontes <pontescpedro@gmail.com>
2026-05-07 11:01:06 -0300

    Add js/ts lsp to neovim

--- nvim/lua/lsp/servers.lua
+++ nvim/lua/lsp/servers.lua
@@ -75,3 +75,21 @@ setup("rust_analyzer", {
     },
   },
 })
+
+setup("ts_ls", {
+  cmd = { "typescript-language-server", "--stdio" },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+  },
+  root_markers = {
+    "tsconfig.json",
+    "jsconfig.json",
+    "package.json",
+    ".git",
+  },
+})
