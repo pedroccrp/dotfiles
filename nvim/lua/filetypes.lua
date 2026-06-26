c2b05e60b8f0f8d72c8d2386967ab9b78322b98b
Pedro Pontes <pontescpedro@gmail.com>
2026-06-26 10:10:33 -0300

    Improve filetype recognition on neovim

--- nvim/lua/filetypes.lua
+++ nvim/lua/filetypes.lua
@@ -1,31 +1,16 @@
-local filetype_map = {
-  text = { "txt", "log" },
-  markdown = { "md", "markdown" },
-  python = { "pyw" },
-  javascript = { "jsx" },
-  javascriptreact = { "jsx" },
-  typescript = { "tsx" },
-  typescriptreact = { "tsx" },
-  html = { "htm" },
-  tmux = { "tmux.conf" },
-  gdscript = { "gd", "gdscript", "gdscript3" },
-  kotlin = { "kt" },
-}
-
-local function set_filetype()
-  local filename = vim.fn.expand("%:t")
-
-  for filetype, extensions in pairs(filetype_map) do
-    for _, ext in ipairs(extensions) do
-      if filename:sub(-#ext) == ext then
-        vim.bo.filetype = filetype
-        return
-      end
-    end
-  end
-end
-
-vim.api.nvim_create_autocmd("BufRead", {
-  pattern = "*",
-  callback = set_filetype,
+vim.filetype.add({
+  extension = {
+    tsx = "typescriptreact",
+    jsx = "javascriptreact",
+    pyw = "python",
+    htm = "html",
+    kt = "kotlin",
+    gd = "gdscript",
+    gdscript3 = "gdscript",
+    -- "log" and "md" are handled by default, but you can add them if needed:
+    log = "text",
+  },
+  filename = {
+    ["tmux.conf"] = "tmux",
+  },
 })
