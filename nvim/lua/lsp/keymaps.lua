4e60ce0ba39d48787a0b741f189c369acfd21dc2
Pedro Pontes <pontescpedro@gmail.com>
2026-08-07 14:17:20 -0300

    Use vim lsb to go to definition instead of telescope

--- nvim/lua/lsp/keymaps.lua
+++ nvim/lua/lsp/keymaps.lua
@@ -1,7 +1,6 @@
 local ok, telescope = pcall(require, "telescope.builtin")
 
 local refs = ok and telescope.lsp_references or vim.lsp.buf.references
-local defs = ok and telescope.lsp_definitions or vim.lsp.buf.definition
 local impl = ok and telescope.lsp_implementations or vim.lsp.buf.implementation
 
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -14,7 +13,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
     vim.keymap.set("n", "grr", refs, opts)
-    vim.keymap.set("n", "gd", defs, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "gi", impl, opts)
 
     vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
