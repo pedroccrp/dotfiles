e8ab27fa4deb89e4556e6a89576aefacbe7af7ef
Pedro Pontes <pontescpedro@gmail.com>
2026-08-07 14:13:34 -0300

    Update treesitter configs and enable auto install

--- nvim/after/plugin/core/treesitter.lua
+++ nvim/after/plugin/core/treesitter.lua
@@ -1,43 +1,80 @@
 local helpers = require("helpers")
 
 local treesitter = helpers.safe_require("nvim-treesitter")
-local treesitter_config = helpers.safe_require("nvim-treesitter.config")
 local ibl = helpers.safe_require("ibl")
 local ibl_hooks = helpers.safe_require("ibl.hooks")
 
-if not treesitter or not treesitter_config or not ibl or not ibl_hooks then return end
+if not treesitter or not ibl or not ibl_hooks then
+  return
+end
 
-treesitter.setup({
-  ensure_installed = {
-    "vimdoc",
-    "vim",
-    "query",
-    "regex",
-    "javascript",
-    "typescript",
-    "c",
-    "kotlin",
-    "python",
-    "lua",
-    "rust",
-    "ruby",
-    "markdown",
-    "markdown_inline",
-    "zsh",
-    "bash",
-    "go",
-    "html",
-    "css",
-    "json",
-    "yaml",
-    "toml",
-  },
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+local parsers = {
+  "vimdoc",
+  "vim",
+  "query",
+  "regex",
+  "javascript",
+  "typescript",
+  "c",
+  "kotlin",
+  "python",
+  "lua",
+  "rust",
+  "ruby",
+  "markdown",
+  "markdown_inline",
+  "zsh",
+  "bash",
+  "go",
+  "html",
+  "css",
+  "json",
+  "yaml",
+  "toml",
+  "starlark",
+}
+
+treesitter.install(parsers)
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("TreesitterAutoInstall", {
+    clear = true,
+  }),
+
+  callback = function(args)
+    local filetype = vim.bo[args.buf].filetype
+
+    if filetype == "" then
+      return
+    end
+
+    local language = vim.treesitter.language.get_lang(filetype) or filetype
+    local available = treesitter.get_available()
+
+    -- Ignore synthetic plugin filetypes such as "notify".
+    if not vim.tbl_contains(available, language) then
+      return
+    end
+
+    local function start()
+      if not vim.api.nvim_buf_is_valid(args.buf) then
+        return
+      end
+
+      pcall(vim.treesitter.start, args.buf, language)
+    end
+
+    local installed = treesitter.get_installed()
+
+    if vim.tbl_contains(installed, language) then
+      start()
+      return
+    end
+
+    treesitter.install({ language }):await(function()
+      vim.schedule(start)
+    end)
+  end,
 })
 
 local hooks = ibl_hooks
