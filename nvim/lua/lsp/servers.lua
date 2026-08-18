4ed6fcb4b396ad0de04ec014c23c3a91b35d7fdc
Pedro Pontes <pontescpedro@gmail.com>
2026-08-18 13:17:23 -0300

    Make lua recognize neovim files

--- nvim/lua/lsp/servers.lua
+++ nvim/lua/lsp/servers.lua
@@ -27,6 +27,12 @@ setup("lua_ls", {
       diagnostics = {
         globals = { "vim" },
       },
+      runtime = {
+        version = "LuaJIT",
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
     },
   },
 })
